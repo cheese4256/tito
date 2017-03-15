3a9fc3abe2534eaa28d5e19cb12ee57d385be764
Cheese <jchisholm@nevo.com>
2017-03-15 11:51:30 -0400

    Made sure we redirect root appropriately based on authentication status.

--- app/src/home/home.component.ts
+++ app/src/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Headers, Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 // service imports
+import { JwtService } from '../common/service/jwt.service';
 import { SausageService } from '../common/service/sausage.service';
 
 @Component({
@@ -12,7 +14,13 @@ export class HomeComponent {
   public test: string;
 
   public constructor(private _http: Http,
-    private _sausageService: SausageService) {}
+    private _sausageService: SausageService,
+    private _jwtService: JwtService,
+    private _router: Router) {
+    if (!this._jwtService.isAuthenticated()) {
+      this._router.navigate(['login']);
+    }
+  }
 
   public getTestData(): void {
     this._sausageService.getTestData().then((r) => this.test = r);
